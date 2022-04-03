3cab6c5e940e46b31560b2fa91be4d5535e68d92
DM <difermu02@gmail.com>
2022-04-03 16:40:50 -0500

    subir archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
-
+import { Persona } from './modelo/persona';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'persona';
+
+  personaArray: Persona[] =[
+    {id: 1, nombre: "Adrian", edad: 20 },
+    {id: 2, nombre: "Natalia", edad: 24},
+    {id: 3, nombre: "Johan", edad: 17 },
+  ];
+
+  seleccionPersona: Persona = new Persona();
+
+  editar(persona: Persona){
+    this.seleccionPersona = persona;
+  }
+
+  agregar_editar(){
+    if (this.seleccionPersona.id == 0) {
+      this.seleccionPersona.id = this.personaArray.length + 1;
+      this.personaArray.push(this.seleccionPersona);
+    }
+    this.seleccionPersona = new Persona();  
+  }
+
+  borrar() {
+    this.personaArray = this.personaArray.filter(elemento => elemento != this.seleccionPersona);
+    this.seleccionPersona = new Persona();
+  }
+
 }
+
+
